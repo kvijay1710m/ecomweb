6f89da487105d4f02faac2c346c592a8f66b736b
kvijay1710 <Vijay@Pixalive.me>
2024-09-05 17:16:06 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express"
 import  products from "./data/products.js"
-
+import cors from "cors"
 
 const port = 5000
 
 const app = express()
+
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("Hello")
